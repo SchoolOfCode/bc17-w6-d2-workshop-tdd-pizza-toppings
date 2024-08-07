f340661b1f245b29595a6ef31ea6f33fd8c35cff
Carmen <aisofiacatarino@gmail.com>
2024-08-07 10:22:15 +0100

    Tocket 3 we made test pass for a 2 letter word (TO).

--- scrabble-score.js
+++ scrabble-score.js
@@ -1,4 +1,34 @@
 //implementation will go here
+const letters = {
+  // moved this to the top
+  A: 1,
+  B: 3,
+  C: 3,
+  D: 2,
+  E: 1,
+  F: 4,
+  G: 2,
+  H: 4,
+  I: 1,
+  J: 8,
+  K: 5,
+  L: 1,
+  M: 3,
+  N: 1,
+  O: 1,
+  P: 3,
+  Q: 10,
+  R: 1,
+  S: 1,
+  T: 1,
+  U: 1,
+  V: 4,
+  W: 4,
+  X: 8,
+  Y: 4,
+  Z: 10,
+  TO: 11,
+};
 export function calculateScrabbleScore(word) {
   switch (word) {
     case "A":
@@ -55,11 +85,18 @@ export function calculateScrabbleScore(word) {
       return 10;
     case "TO":
       return 11;
+    case "TO": //moved this case to here instead of bellow of what we were doing bellow
+      return ["T"] + ["O"];
   }
 }
 
-
-
+/* export function calculateScrabbleScore(firstLetter, secondLetter) {
+    switch (firstLetter) {
+case "TO":
+  return ['T'] + ['O'];
+    }
+  }
+*/
 /*export default function calculate(operator, firstNumber, secondNumber) {
   switch (operator) {
     case "+":
@@ -77,39 +114,3 @@ export function calculateScrabbleScore(word) {
       throw new Error(`Unsupported operator ${operator}`);
   }
 } */
-
-const letters = [
-   "A"= 1,
-   "B"= 3,
-   "C"= 3,
-   "D"= 2,
-   "E"= 1,
-   "F"= 4,
-   "G"= 2,
-   "H"= 4,
-   "I"= 1,
-   "J"= 8,
-   "K"= 5,
-   "L"= 1,
-   "M"= 3,
-   "N"= 1,
-   "O"= 1,
-   "P"= 3,
-   "Q"= 10,
-   "R"= 1,
-   "S"= 1,
-   "T"= 1,
-   "U"= 1,
-   "V"= 4,
-   "W"= 4,
-   "X"= 8,
-   "Y"= 4,
-   "Z"= 10,
-   "TO"= 11,
-]
-
-  export function calculateScrabbleScore(firstLetter, secondLetter) {
-    switch (firstLetter) {
-
-    }
-  }
